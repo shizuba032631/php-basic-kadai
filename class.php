83d8055355248c576fae29bcbc2f1aec2ee22201
shizuba032631 <158788508+shizuba032631@users.noreply.github.com>
2024-05-19 20:43:03 +0900

    Add files via upload

--- class.php
+++ class.php
@@ -15,31 +15,46 @@
              public $name;
              public $price;
              // メソッドを定義する
-             public function show_price(string $name) {
-                $this->name = $name;
+             public function show_name() {
+                echo $this->name;
+             }
+             public function show_price() {
+                echo $this->price;
+             }
             }
-         }
+         
          class Animal {
             // プロパティ$name,$height,$weightを定義する
             public $name;
             public $height;
             public $weight;
             // メソッドを定義する
-            public function show_height(string $height) {
-               $this->height = $height;
+            public function show_name() {
+                echo $this->name;
+           }
+           public function show_height() {
+                echo $this->height;
+           }
+           public function show_weight() {
+                echo $this->weight;
            }
         }
- 
          // インスタンス化する
          $humburg = new Food();
          $cat = new Animal();
          // プロパティにアクセスし、値を代入する
          $humburg->name = 'ハンバーグ' . '<br>';
+         $humburg->price = '500円'. '<br>';
+         $cat->name = '猫';
+         $cat->height = '25cm';
          $cat->weight = '1000g' . '<br>';
-         // プロパティにアクセスし、値を出力する
-         print_r($humburg->name);
-         print_r($cat->weight);
-         ?>
+
+         // メソッドを使ってプロパティの値を出力する
+        print_r($humburg->name);
+        print_r($cat->weight);
+        
+        ?>
+         
      </p>
  </body>
  
